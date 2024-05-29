8f57c0183e1e5cf8d58f2989dd8333ac01a71e35
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-05-29 11:36:56 +0530

    added responsiveness

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -2,24 +2,24 @@
 function Expertise() {
 
     return (
-        <div id="expertise" className="flex flex-col items-center justify-center p-10 mb-60 bg-gradient-to-r from-[#004c84] via-[#245f85] to-[#004276]  relative top-[-10vh]">
-            <div className="text-8xl font-mono font-bold text-[#FFFF] mt-5">
+        <div id="expertise" className="flex flex-col items-center justify-center bg-gradient-to-r from-[#00304f] via-[#0d364d] to-[#002440] h-fit">
+            <div className="text-5xl md:text-8xl font-mono font-bold text-[#FFFF] mt-5">
                 <span>My Expertise</span>
             </div>
 
-            <div className="flex justify-center m-10 text-[#FFFF]">
-                <div className="border-4 w-3/12 p-10 min-w-min mr-0.5">
+            <div className="flex flex-wrap md:flex md:flex-wrap justify-center m-10 text-[#FFFF]">
+                <div className="text-xl md:text-3xl border-4 w-full m-2 md:w-1/4 p-10 min-w-min">
                     <div>
-                        <span className="text-3xl">Software Development</span>
+                        <span className="">Software Development</span>
                     </div>
-                    <p className="mt-3 font-sans text-xl text-[#d1b2f0]">Skilled in procedural and Object-Oriented Programming languages like Rust, Python, Go, Java, and TypeScript.</p>
+                    <p className="mt-3 font-sans text-base md:text-xl text-[#d1b2f0]">Skilled in procedural and Object-Oriented Programming languages like Rust, Python, Go, Java, and TypeScript.</p>
                 </div>
 
-                <div className="border-4 w-3/12 p-10 min-w-min text-[#FFFF]">
+                <div className="text-xl md:text-3xl border-4 w-full m-2 md:w-1/4 p-10 min-w-min text-[#FFFF]">
                     <div>
-                        <span className="text-3xl">Backend Development: Golang, Node.js, Rust, Python</span>
+                        <span className="">Backend Development: Golang, Node.js, Rust, Python</span>
                     </div>
-                    <p className="mt-3 font-sans text-xl text-[#d1b2f0]">Enthusiastic about microservices, backend systems, networking, cloud computing, and Linux. With over a year of experience, I specialize in designing robust backend architectures and developing scalable microservices.</p>
+                    <p className="mt-3 font-sans text-base md:text-xl text-[#d1b2f0]">Enthusiastic about microservices, backend systems, networking, cloud computing, and Linux. With over a year of experience, I specialize in designing robust backend architectures and developing scalable microservices.</p>
                 </div>
             </div>
         </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,19 @@
 
 function NavBar() {
     return (
-        <nav className="flex justify-between p-10">
-            <div className="text-4xl w-2/5 text-start min-w-fit">
+        <nav className="bg-[#0000006b]  md:flex justify-between p-10">
+            <div className="text-center text-4xl w-full md:w-2/5 md:text-start min-w-fit">
                 <span className="text-[#c84576]">2k4sm <strong>.</strong></span>
                 <span className="text-[#1c7ea1] font-medium"> _</span>
             </div>
 
-            <div className="flex justify-start w-3/5 font-mono font-bold">
-                <p className="m-4 text-[#c74b7c]"><a href="#home">// home</a></p>
-                <p className="m-4 text-[#b95093]"><a href="#expertise">// expertise</a></p>
-                <p className="m-4 text-[#a56498]"><a href="/work">// work</a></p>
-                <p className="m-4 text-[#9b4d9b]"><a href="/experience">// experience</a></p>
-                <p className="m-4 text-[#5a2f83]"><a href="/contact">// contact</a></p>
-            </div>
+            <ul className="flex justify-center md:justify-start items-center w-1/3 mx-auto md:w-3/5 font-mono font-bold list-none">
+                <li className="w-fit m-1 sm:m-4 text-[#c74b7c]"><a href="#home">// home</a></li>
+                <li className="w-fit m-1 sm:m-4 text-[#b95093]"><a href="#expertise">// expertise</a></li>
+                <li className="w-fit m-1 sm:m-4 text-[#a56498]"><a href="/work">// work</a></li>
+                <li className="w-fit m-1 sm:m-4 text-[#9b4d9b]"><a href="/experience">// experience</a></li>
+                <li className="w-fit m-1 sm:m-4 text-[#5a2f83]"><a href="/contact">// contact</a></li>
+            </ul>
         </nav>
     )
 }
